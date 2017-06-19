0a01dc417d3fd138376b4b228acf267f94697fc2
Jackal0024 <icemaunntenn@gmail.com>
2017-06-19 19:45:11 +0900

    SQLのヒント追加　ランキング画面作成

--- Ranking.php
+++ Ranking.php
@@ -3,15 +3,25 @@ $dsn = 'mysql:dbname=WareHouseDB;host=127.0.0.1';
 $user = 'root';
 $pw = 'Neec2017!';
 
-$sql = 'SELECT * FROM Ranking';
+ $sql = "insert into Ranking values(".$_POST['score'].")";
 
-//SQLを実⾏
+// //SQLを実⾏
 $dbh = new PDO($dsn, $user, $pw); //接続
 $sth = $dbh->prepare($sql); //SQL準備
 $sth->execute(); //実⾏
 
-//結果を取得
-while( ($buff = $sth->fetch()) !== false ){
- print implode(', ', $buff);
- print "¥n";
+$sql = 'select * from Ranking ORDER BY score';
+$sth = $dbh->prepare($sql); //SQL準備
+$sth->execute(); //実⾏
+
+
+$i = 0;
+$file = 'Ranking.txt';
+$current = '';
+while( ($buff = $sth->fetch()) !== false && $i < 10){
+     $current .= $buff['score'];
+     $current .= ',';
+     $i++;
 }
+echo $current;
+//file_put_contents($file, $current);
